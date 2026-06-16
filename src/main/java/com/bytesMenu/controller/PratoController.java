e9ef16ee3da902de1969846abb448cf469df0e99
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-06-16 13:58:47 -0300

    update pratoController

--- src/main/java/com/bytesMenu/controller/PratoController.java
+++ src/main/java/com/bytesMenu/controller/PratoController.java
@@ -1,10 +1,9 @@
 package com.bytesMenu.controller;
 
-import com.bytesMenu.entity.Prato;
 import com.bytesMenu.dto.PratoRequestDTO;
+import com.bytesMenu.entity.Prato;
 import com.bytesMenu.service.PratoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,41 +11,59 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/apipratos")
+@RequestMapping("/pratos")
+@RequiredArgsConstructor
 public class PratoController {
 
-    @Autowired
-    private PratoService pratoService;
+    private final PratoService pratoService;
 
     @PostMapping
-    public ResponseEntity<Prato> criar(Prato dto) {
-        Prato prato = pratoService.criar(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(prato);
+    public ResponseEntity<Prato> criar(@RequestBody PratoRequestDTO dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(pratoService.criar(dto));
     }
 
     @GetMapping("/listar-todos")
     public ResponseEntity<List<Prato>> listarTodos() {
+
         return ResponseEntity.ok(pratoService.listarTodos());
     }
 
     @GetMapping("/disponiveis")
     public ResponseEntity<List<Prato>> listarDisponiveis() {
-        return ResponseEntity.ok(pratoService.listarTodos());
+
+        return ResponseEntity.ok(pratoService.listarDisponiveis());
     }
 
-    @GetMapping("/{id")
+    @GetMapping("/{id}")
     public ResponseEntity<Prato> buscarPorId(@PathVariable Long id) {
+
         return ResponseEntity.ok(pratoService.buscarPorId(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Prato> atualizar(@PathVariable Long id, @RequestBody Prato prato) {
-        return ResponseEntity.ok(pratoService.atualizar(id, prato));
+    public ResponseEntity<Prato> atualizar(
+            @PathVariable Long id,
+            @RequestBody PratoRequestDTO dto) {
+
+        return ResponseEntity.ok(pratoService.atualizar(id, dto));
+    }
+
+    @PatchMapping("/{id}/disponivel")
+    public ResponseEntity<Prato> alterarDisponibilidade(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                pratoService.alterarDisponibilidade(id)
+        );
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Prato> deletar(@PathVariable Long id) {
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+
         pratoService.deletar(id);
+
         return ResponseEntity.noContent().build();
     }
 }
